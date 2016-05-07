af5adda21f6fb0009519159a798aed5dd683f2f5
Ivan Verhun <ivanver@jfrog.com>
2016-05-08 01:19:40 +0300

    added delete and undo endpoints. Added delete support on UI. Improved counters

--- src/main/java/test/photos/HelloController.java
+++ src/main/java/test/photos/HelloController.java
@@ -1,41 +1,72 @@
 package test.photos;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.Set;
+import java.util.Stack;
 import java.util.stream.Collectors;
 
-/**
- * TODO: Document me
- *
- * @author Ivan Verhun (ivanver@jfrog.com)
- */
 @RestController
 public class HelloController {
 
     public static final String PATH = "/Users/ivanverhun/Pictures/Photos Library.photoslibrary/Masters/2016/05/03/20160503-195853/";
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
+    private final Set<String> removedPhotos = new HashSet<>();
+    private final Stack<UpdateCommand> updateCommands = new Stack<>();
 
     @RequestMapping("/photos")
     public List<PhotosList> photos() throws IOException {
         return Files.list(Paths.get(PATH))
                     .filter(p -> !p.getFileName().toString().endsWith("MOV"))
                     .limit(10)
-                    .map(PhotosList::new).collect(Collectors.toList());
+                    .map(PhotosList::new)
+                    .filter(pl -> !removedPhotos.contains(pl.desc))
+                    .collect(Collectors.toList());
+    }
+
+    @RequestMapping(value = "/photos", method = RequestMethod.PUT, consumes = "application/json")
+    public void mark(@RequestBody UpdateCommand updateCommand) throws IOException {
+        markPhoto(updateCommand.imgPath, updateCommand.action);
+        updateCommands.push(updateCommand);
+    }
+
+    @RequestMapping(value = "/photos/undo", method = RequestMethod.POST)
+    public void undo() throws IOException {
+        UpdateCommand updateCommand = updateCommands.pop();
+        markPhoto(updateCommand.imgPath, updateCommand.action.getUndoAction());
+    }
+
+    private void markPhoto(String imgPath, Action action) {
+        if (Action.DELETE.equals(action)) {
+            removedPhotos.add(imgPath);
+        } else {
+            removedPhotos.remove(imgPath);
+        }
+    }
+
+    public static class UpdateCommand {
+        public String imgPath;
+        public Action action;
+    }
+
+    public enum Action {
+        DELETE, RESTORE;
+
+        public Action getUndoAction() {
+            if (this.equals(DELETE)) {
+                return RESTORE;
+            } else {
+                return DELETE;
+            }
+        }
     }
 
 }
--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -23,16 +23,39 @@ angular.module('example366', ['ngAnimate', 'ngTouch', 'cfp.hotkeys'])
             }
         });
 
+
+        hotkeys.add({
+            combo: '1',
+            description: 'Remove 1st photo',
+            callback: function() {
+                console.log("1 pressed");
+                $scope.delete($scope._Index1);
+            }
+        });
+
+        hotkeys.add({
+            combo: '2',
+            description: 'Remove 2nd photo',
+            callback: function() {
+                console.log("2 pressed");
+                $scope.delete($scope._Index2);
+            }
+        });
+
         // Set of Photos
         $scope.photos = [];
-
-        $http.get('http://localhost:8080/photos').then(function (response) {
-            $scope.photos = response.data;
-        });
 
         // initial image index
         $scope._Index1 = 0;
         $scope._Index2 = 1;
+
+        $scope.loadPhotos = function () {
+            $http.get('http://localhost:8080/photos').then(function (response) {
+                $scope.photos = response.data;
+            });
+        }
+
+        $scope.loadPhotos();
 
         // show prev image
         $scope.moveToPrev = function () {
@@ -48,6 +71,19 @@ angular.module('example366', ['ngAnimate', 'ngTouch', 'cfp.hotkeys'])
 
         $scope.total = function () {
             return $scope.photos.length
+        }
+
+        $scope.delete = function (index) {
+            var data = {
+                imgPath : $scope.photos[index].desc,
+                action : "DELETE"
+            };
+
+            $http.put('http://localhost:8080/photos', data).then(function (response) {
+                console.log("Deleted " + $scope.photos[index]);
+            });
+
+            $scope.loadPhotos();
         }
 
         var next = function (index) {
